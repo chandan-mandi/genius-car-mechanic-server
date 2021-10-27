91e611c3f0d4c7520ecb85e0c365be59bf548d98
chandan-mandi <itscoderchandan@gmail.com>
2021-10-28 01:39:13 +0530

    api update

--- index.js
+++ index.js
@@ -68,6 +68,10 @@ app.get('/', (req, res) => {
     res.send('Running Genius Server');
 })
 
+app.get('/hello', (req, res) => {
+    res.send('hello update here')
+})
+
 app.listen(port, () => {
     console.log('Running Genius server on port', port)
 })
